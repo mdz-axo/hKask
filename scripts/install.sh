36c33657683bf6d8ceab04723f06ba26ca16ce56
mdz-axo <mdz@axolotl.partners>
2026-06-04 09:28:02 -0700

    ci build

--- scripts/install.sh
+++ scripts/install.sh
@@ -192,7 +192,7 @@ install_rust() {
         local rust_version=$(rustc --version)
         log "Rust already installed: $rust_version"
 
-        if ! rustc --version | grep -qE 'rustc [0-9]+\.(8[5-9]|9[0-9]|[1-9][0-9]{2,})\.'; then
+        if ! rustc --version | grep -qE 'rustc [0-9]+\.(8[5-9]|9[0-9]|[0-9]{2,})\.'; then
             log_warning "Rust version too old for edition 2024 (requires 1.85+). Update with 'rustup update' or install from https://rustup.rs"
         fi
     else
